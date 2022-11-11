e3b687d68d272e1b04aeeb7c4509adf394eb1b8e
mfrfmatheus <115051947+mfrfmatheus@users.noreply.github.com>
2022-11-10 21:01:54 -0300

    Add files via upload

---
+++ ER_UC8 JAVA.js
@@ -0,0 +1,25 @@
+var listadepeca
+
+listadepeca = ["Filtro de Ar", "Amortecedor", "Disco de freio"]
+
+if (listadepeca.length<10){
+    console.log("e possivel cadastrar mais pecas")
+}else{
+    console.log("nao tem mais espaco na lista")
+}
+
+let peso
+peso = 300
+if (peso<100){
+    console.log("a peça precisa ter no mínimo 100g")
+    }else{
+    console.log("a peça possui peso adequado")
+    }
+
+    let nomepeca
+    nomepeca = "Caixa de Cambio"
+if (nomepeca.length>3){
+console.log("O tamanho do nome da peca esta adequado")
+}else{
+console.log("o nome deve ter mais que 3 caracateres, digite um nome adequado")
+}
